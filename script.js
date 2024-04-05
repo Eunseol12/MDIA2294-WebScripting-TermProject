fc41755ce5545e38e1737f5f65e62bd3348c2b19
Eunseol12 <154369841+Eunseol12@users.noreply.github.com>
2024-04-05 09:24:34 -0700

    add image slider
    flickity plug in but not working ... why????????????????????????????????????????????????????? :(

--- script.js
+++ script.js
@@ -1,34 +1,13 @@
-const testimonialArray = [
-    "testimonials/testimonial1.png",
-    "testimonials/testimonial2.png",
-    "testimonials/testimonial3.png",
-    "testimonials/testimonial4.png",
-    "testimonials/testimonial5.png"
-];
-
-
-const first_load=() => {
-    set_main_image (0);
-}
-
-const set_main_image = (index) => {
-    document.getElementById('main-image').src = testimonialArray[index];
-    document.getElementById('current-index').value = index;
-}
-
-
-const set_nav_first = ()=> {
-    set_main_image(0);
-}
-
-const set_nav_last = () => {
-    set_main_image(testimonialArray.length -1);
-}
-
-const set_nav_next = () => {
-}
-
-window.addEventListener("load", first_load);
-
-let prev_btn = document.getElementById('prev');
-prev.addEventListener('click',)
+document.addEventListener('DOMContentLoaded', function () {
+    var elem = document.querySelector('.main-carousel');
+    var flkty = new Flickity(elem, {
+        cellAlign: 'left',
+        contain: true,
+        pageDots: true,
+        pageDots: true,
+        prevNextButtons: false,
+        autoPlay: true,
+        autoPlay: 5000,
+        wrapAround: true
+    });
+});

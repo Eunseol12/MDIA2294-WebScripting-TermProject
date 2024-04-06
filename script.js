3b68a732551bb5a160fcd483fe2456b87ba3ef6d
Eunseol12 <154369841+Eunseol12@users.noreply.github.com>
2024-04-06 16:53:59 -0700

    add contact

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var elem = document.querySelector('.main-carousel');
+    var elem = document.querySelector('#main-carousel');
+    var flkty = new Flickity(elem, {
+        cellAlign: 'left',
+        contain: true,
+        pageDots: true,
+        pageDots: true,
+        prevNextButtons: false,
+        autoPlay: 5000,
+        wrapAround: true
+    });
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elem = document.querySelector('#testimonial-carousel');
     var flkty = new Flickity(elem, {
         cellAlign: 'left',
         contain: true,

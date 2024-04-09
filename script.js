beec0150bcf91065e4824ce4d933b6ff52882e81
Eunseol12 <154369841+Eunseol12@users.noreply.github.com>
2024-04-08 22:32:57 -0700

    plugin
    add
    - map
    - sign up modal
    - masonry (images)
    - flickity
    
    still need to be fixed
    
    delete accessories page

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//modal
+$(document).ready(function(){
+    $('#loginModal').modal({
+      backdrop: 'static',
+      keyboard: false
+    });
+  });
 document.addEventListener('DOMContentLoaded', function () {
     var elem = document.querySelector('#main-carousel');
     var flkty = new Flickity(elem, {
@@ -10,6 +17,43 @@ document.addEventListener('DOMContentLoaded', function () {
         wrapAround: true
     });
 });
+
+
+//men, women, accessory
+document.addEventListener('DOMContentLoaded', function () {
+    var productHoodie = new Masonry ('#product_hoodie', {
+        itemSelector:'.hoodie_details',
+        columnWidth:'.hoodie_details',
+        gutter:16,
+    });
+
+    var productT = new Masonry ('#product_t', {
+        itemSelector:'.t_details',
+    });
+
+    var productHoodie = new Masonry ('#product_shirts', {
+        itemSelector:'.shirts_details',
+    });
+
+    var productT = new Masonry ('#product_pants', {
+        itemSelector:'.pants_details',
+    });
+
+});
+
+
+// contact us
+
+var map = L.map('map').setView([0,0], 2);
+var bcit = [49.17, 123.5];
+ 
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map); 
+
+L.marker(bcit).addTo(map)
+    .bindPopup('We are here')    
+    .openPopup();
 
 
 
